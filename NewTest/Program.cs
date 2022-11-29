a3616fb27315ada15e315425ed756243f919b6c7
Denis <denis13071981@gmail.com>
2022-11-29 04:18:34 +0300

    add test

---
+++ NewTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+Regex reg = new Regex("</?.*?>");
+StreamReader file = new StreamReader("Readme.md");
+string input = file.ReadToEnd();
+input = reg.Replace(input, "").Replace(",", " ").Replace("  ", " ").Replace("\"", " ").Replace("    ", " ");
+string[] line = input.Split(" ");
+
+Console.Write(String.Join(",", line.Where(s => s.Length <= 3)));
+
+
