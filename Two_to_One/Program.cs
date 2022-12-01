54a467db6554aad3b8cf5e0c67940aba609e3e61
Denis <denis13071981@gmail.com>
2022-12-02 01:30:18 +0300

    Two to One

---
+++ Two_to_One/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// test
+Console.WriteLine(Longest("aretheyhere", "yestheyarehere"));
+Console.WriteLine(Longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+Console.WriteLine(Longest("inmanylanguages", "theresapairoffunctions"));
+Console.WriteLine(Longest("lordsofthefallen", "gamekult"));
+Console.WriteLine(Longest("codewars", "codewars"));
+Console.WriteLine(Longest("agenerationmustconfrontthelooming", "codewarrs"));
+
+
+// deciion
+string Longest (string s1, string s2) => string.Concat((s1 + s2).ToLower().Distinct().OrderBy(x => x));
+    
