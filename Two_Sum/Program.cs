b70343100d79002c26f392df2aa1139d6bf41cab
Denis <denis13071981@gmail.com>
2022-12-02 01:59:17 +0300

    Two Sum

---
+++ Two_Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// test
+Console.WriteLine(String.Join(" ",TwoSum(new [] { 1, 2, 3 }, 4)));
+Console.WriteLine(String.Join(" ",TwoSum(new [] { 1234, 5678, 9012 }, 14690)));
+Console.WriteLine(String.Join(" ",TwoSum(new [] { 2, 2, 3 }, 4)));
+
+// decision
+int[] TwoSum(int[] numbers, int target)
+  {
+    int length = numbers.Length;
+     for (int i = 0; i < length; i++)
+         for (int j = i + 1; j < length; j++)
+             if (numbers[i] + numbers[j] == target)
+             {
+                return new int[]{i,j};
+             }
+                
+     return new int[0];
+  }
