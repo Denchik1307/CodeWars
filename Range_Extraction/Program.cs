c1a98dbfb9ebfc36b0f63c5940c93715d8d50151
Denis <denis13071981@gmail.com>
2022-12-02 02:19:38 +0300

    Range Extraction

--- Range_Extraction/Program.cs
+++ Range_Extraction/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// test
+Console.WriteLine(Extract(new[] { 1, 2 }));
+Console.WriteLine(Extract(new[] { 1, 2, 3 }));
+Console.WriteLine(Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 }));
+Console.WriteLine(Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 }));
+
+
+
+// decision
+string Extract(int[] args)
+{
+    string output = string.Empty;
+    int temp;
+    int ie = 0;
+    while (ie < args.Length)
+    {
+        if (args.Contains(args[ie] + 1) && args.Contains(args[ie] + 2))
+        {
+            output = output + $"{args[ie]}-";
+            ie++;
+            temp = args[ie];
+            while (args.Contains(temp + 1))
+            {
+                temp++;
+                ie++;
+            }
+            output += $"{temp},";
+            ie++;
+        }
+        else
+        {
+            output = output + $"{args[ie]},";
+            ie++;
+        }
+    }
+    output = output.TrimEnd(',');
+    return output;
+}
