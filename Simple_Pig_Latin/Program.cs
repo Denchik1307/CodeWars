c029cb5c46db0252aa726aca54a81da4b36c7f1e
Denis <denis13071981@gmail.com>
2022-12-02 01:39:24 +0300

    Simple Pig Latin

---
+++ Simple_Pig_Latin/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+// test
+Console.WriteLine(PigIt("Pig latin is cool"));
+Console.WriteLine(PigIt("This is my string"));
+Console.WriteLine(PigIt("Hello world !"));
+
+
+// decision
+string PigIt(string str)
+  {
+    string[] words = str.Split(' ');
+    for (int i = 0; i < words.Length; i++)
+    {
+        words[i] = words[i][1..^0] + words[i][0];
+        if(words[i] != "!") words[i]+= ((i == words.Length - 1) ? "ay" : "ay ");
+        
+    }
+    str = "";
+    foreach (string i in words)
+    {
+        str += i;
+    }
+
+    return str;
+  }
