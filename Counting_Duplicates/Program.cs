ac4af50ecb4a6dcd2539833115deb0f556b9e840
Denis <denis13071981@gmail.com>
2022-12-02 01:03:00 +0300

    Counting Duplicates

---
+++ Counting_Duplicates/Program.cs
@@ -0,0 +1,14 @@
+﻿// using System.Collections.Generic;
+// using System.Linq;
+
+// test
+Console.WriteLine(DuplicateCount(""));
+Console.WriteLine(DuplicateCount("abcde"));
+Console.WriteLine(DuplicateCount("aabbcde"));
+Console.WriteLine(DuplicateCount("aabBcde"));
+Console.WriteLine(DuplicateCount("Indivisibility"));
+Console.WriteLine(DuplicateCount("Indivisibilities"));
+
+
+// decision
+int DuplicateCount(string str) => str.ToLower().GroupBy(i => i).Count(j => j.Count() > 1);
