d14665f4a064a38b8436ba125a901ea2f4a7206b
Denis <denis13071981@gmail.com>
2022-12-02 02:14:12 +0300

    Valid Braces

---
+++ Valid_Braces/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+// test
+Console.WriteLine(validBraces("()"));
+Console.WriteLine(validBraces("[]"));
+Console.WriteLine(validBraces("(){}[]"));
+Console.WriteLine(validBraces("([{}])"));
+Console.WriteLine(validBraces("([{}])"));
+Console.WriteLine(validBraces("[(])"));
+Console.WriteLine(validBraces("({})[({})]"));
+Console.WriteLine(validBraces("(})"));
+Console.WriteLine(validBraces("{}"));
+Console.WriteLine(validBraces("(({{[[]]}}))"));
+Console.WriteLine(validBraces("{}({})[]"));
+Console.WriteLine(validBraces(")(}{]["));
+Console.WriteLine(validBraces("())({}}{()][]["));
+Console.WriteLine(validBraces("(((({{"));
+Console.WriteLine(validBraces("}}]]))}])"));
+Console.WriteLine(validBraces("}}()"));
+
+
+// decision
+bool validBraces(String braces)
+{
+    int count = 0;
+    while (braces != "" ^ count == braces.Length * 2)
+    {
+        count++;
+        braces = braces.Replace("()", "").Replace("{}", "").Replace("[]", "");
+    }
+
+    return (braces == "") ? true : false;
+}
