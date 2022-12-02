0048dadfd7940ea1dc6ba3cf65e815108343d0c9
Denis <denis13071981@gmail.com>
2022-12-02 23:28:30 +0300

    Replace With Alphabet Position

---
+++ Replace_With_Alphabet_Position/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// test 
+Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock."));
+Console.WriteLine(AlphabetPosition("The narwhal bacons at midnight."));
+Console.WriteLine(AlphabetPosition("-.-'"));
+
+
+
+// decision
+string AlphabetPosition(string text)
+{
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    text = text.ToLower();
+    int pos;
+    string result = string.Empty;
+    foreach (char item in text)
+    {
+        pos = alphabet.IndexOf(item);
+        if (pos > -1)
+            result += $"{pos + 1} ";
+    }
+
+    return result.TrimEnd();
+}
