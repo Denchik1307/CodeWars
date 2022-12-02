5b50c0a3719b5beb513088ce770f3db1a4ce65a9
Denis <denis13071981@gmail.com>
2022-12-02 23:09:36 +0300

    Regex validate PIN code

---
+++ Regex_validate_PIN_code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+// test
+Console.WriteLine(ValidatePin("1"));
+Console.WriteLine(ValidatePin("12"));
+Console.WriteLine(ValidatePin("123"));
+Console.WriteLine(ValidatePin("12345"));
+Console.WriteLine(ValidatePin("1234567"));
+Console.WriteLine(ValidatePin("-1234"));
+Console.WriteLine(ValidatePin("-12345"));
+Console.WriteLine(ValidatePin("1234"));
+Console.WriteLine(ValidatePin("-1.234"));
+Console.WriteLine(ValidatePin("00000000"));
+
+
+// decision
+bool ValidatePin(string pin) => !new Regex(@"[^0-9]").IsMatch(pin) && (pin.Length == 4 ^ pin.Length == 6) ? true : false;
