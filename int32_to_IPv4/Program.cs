42dfd2d5c7c3d48f77f1f7fdbf588bd723436cfc
Denis <denis13071981@gmail.com>
2022-12-04 16:25:59 +0300

    int32 to IPv4

--- int32_to_IPv4/Program.cs
+++ int32_to_IPv4/Program.cs
@@ -8,8 +8,8 @@ Console.WriteLine(UInt32ToIP(2149583361));
 
 // decision
 string UInt32ToIP(uint ip)
-    {
+{
     IPAddress newIP;
-    IPAddress.TryParse(ip.ToString(), out newIP);
-    return newIP.ToString(); 
-  }
+    if (IPAddress.TryParse(ip.ToString(), out newIP!)) return "-1";
+    return newIP.ToString();
+}
