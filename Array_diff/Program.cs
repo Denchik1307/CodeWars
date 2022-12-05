960ce63d09eae7eded38d394b31a5edae3f4f355
Denis <denis13071981@gmail.com>
2022-12-05 18:17:47 +0300

    Array_diff

---
+++ Array_diff/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+// test 
+Console.WriteLine(String.Join(",", ArrayDiff(new int[] { 1, 2, 3, 3, 2, 3, 4, 3, 2, 1, 2, 3, 4, 5, 6, }, new int[] { 2, 5 })));
+
+
+
+// decision
+int[] ArrayDiff(int[] a, int[] b) => a.Where(s=> !b.Contains(s)).ToArray();
